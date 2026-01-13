4572c0ad15c2bbc828e0fa2714f9e3915d786788
Anthony Super <anthony@noided.media>
2026-01-13 11:20:52 -0700

    Simplify selection

--- noided-validation/lib/Noided/Validation/Internal/Validate/Selection.hs
+++ noided-validation/lib/Noided/Validation/Internal/Validate/Selection.hs
@@ -1,18 +1,16 @@
 module Noided.Validation.Internal.Validate.Selection where
 
-import Data.Typeable (Typeable)
-import Noided.Translate (AsTranslateParam)
 import Noided.Validation.Internal.ValidationError.Selection
 import Noided.Validation.Internal.Validator
 
 -- | Validate that a value is one of the allowed options.
-oneOf :: (Monad m, Eq a, Show a, Ord a, Typeable a, AsTranslateParam a) => [a] -> a -> ValidatorT m ()
+oneOf :: (Foldable f, Monad m, Eq a) => f a -> a -> ValidatorT m ()
 oneOf options val
   | val `elem` options = return ()
-  | otherwise = failNonfatal $ InvalidSelection options val
+  | otherwise = failNonfatal InvalidSelection
 
 -- | Validate that a value is NOT one of the forbidden options.
-noneOf :: (Monad m, Eq a, Show a, Ord a, Typeable a, AsTranslateParam a) => [a] -> a -> ValidatorT m ()
+noneOf :: (Foldable f, Monad m, Eq a) => f a -> a -> ValidatorT m ()
 noneOf forbidden val
-  | val `elem` forbidden = failNonfatal $ ForbiddenSelection forbidden val
+  | val `elem` forbidden = failNonfatal ForbiddenSelection
   | otherwise = return ()
