57213ea054c05c9a8c2b79e9ee32447b3488a81b
Anthony Super <anthony@noided.media>
2026-01-14 13:46:30 -0700

    FromFormSubmission class
    Adds in some basic machinery for form submissions.

--- noided-form/lib/Noided/Form/Internal/FromFormSubmission.hs
+++ noided-form/lib/Noided/Form/Internal/FromFormSubmission.hs
@@ -1,13 +1,16 @@
+{-# LANGUAGE DefaultSignatures #-}
 {-# LANGUAGE DerivingVia #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module Noided.Form.Internal.FromFormSubmission where
 
 import Data.Int
 import Data.Kind (Constraint, Type)
-import Data.Text (Text)
+import Data.Map.Strict qualified as Map
+import Data.Text (Text, pack)
 import Data.Text.Lazy qualified as LT
 import Data.Time
 import Data.UUID.Types (UUID)
@@ -21,10 +24,26 @@ type FromFormSubmission :: FormContentType -> Type -> Constraint
 class FromFormSubmission ct a where
   fromFormSubmission :: FormSubmission ct -> Either Text a
 
+  default fromFormSubmission ::
+    (Generic a, GFromFormSubmission ct (Rep a)) =>
+    FormSubmission ct ->
+    Either Text a
+  fromFormSubmission = gfromFormSubmission
+
 -- | Newtype wrapper used with @ -XDerivingVia @ to derive instances of
 -- 'FromFormSubmission' for types with an instance of 'FromHttpApiData'.
 newtype ViaHttpParam a = ViaHttpParam {getViaHttpParam :: a}
   deriving (Show, Read, Eq, Ord, Generic)
+
+parseAtKey' :: Text -> (FormSubmission contentType -> t) -> FormSubmission contentType -> t
+parseAtKey' k f = \case
+  SubmissionObject m
+    | Just val <- Map.lookup k m -> f val
+  _ -> f SubmissionEmpty
+
+-- | Parse underneath a specified key.
+parseAtKey :: (FromFormSubmission contentType a) => Text -> FormSubmission contentType -> Either Text a
+parseAtKey t = parseAtKey' t fromFormSubmission
 
 instance (FromFormSubmission ct a) => FromFormSubmission ct (Maybe a) where
   fromFormSubmission cs
@@ -82,3 +101,36 @@ deriving via ViaHttpParam LocalTime instance FromFormSubmission ct LocalTime
 deriving via ViaHttpParam ZonedTime instance FromFormSubmission ct ZonedTime
 
 deriving via ViaHttpParam UTCTime instance FromFormSubmission ct UTCTime
+
+-- | Generic-deriving for form submissions.
+class GFromFormSubmission ct rep where
+  genericFromFormSubmission :: FormSubmission ct -> Either Text (rep a)
+
+instance (GFromFormSubmission ct f) => GFromFormSubmission ct (M1 D c f) where
+  genericFromFormSubmission = fmap M1 . genericFromFormSubmission
+
+instance (GFromFormSubmission ct f) => GFromFormSubmission ct (M1 C c f) where
+  genericFromFormSubmission = fmap M1 . genericFromFormSubmission
+
+instance (GFromFormSubmission ct f, GFromFormSubmission ct g) => GFromFormSubmission ct (f :*: g) where
+  genericFromFormSubmission s = (:*:) <$> genericFromFormSubmission s <*> genericFromFormSubmission s
+
+instance (Selector s, FromFormSubmission ct c) => GFromFormSubmission ct (M1 S s (K1 i c)) where
+  genericFromFormSubmission s =
+    let key = pack $ selName (undefined :: M1 S s (K1 i c) p)
+     in M1 . K1 <$> parseAtKey key s
+
+instance GFromFormSubmission ct U1 where
+  genericFromFormSubmission _ = pure U1
+
+gfromFormSubmission ::
+  (Generic a, GFromFormSubmission ct (Rep a)) =>
+  FormSubmission ct ->
+  Either Text a
+gfromFormSubmission = fmap to . genericFromFormSubmission @_ @_ @()
+
+instance
+  (Generic a, GFromFormSubmission ct (Rep a)) =>
+  FromFormSubmission ct (Generically a)
+  where
+  fromFormSubmission = fmap Generically . gfromFormSubmission
