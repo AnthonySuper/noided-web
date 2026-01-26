1b89514d1345ee6dd2ad68247b2fa7e4088d1076
Anthony Super <anthony@noided.media>
2026-01-26 14:27:08 -0700

    Removed $

--- noided-sql/lib/Noided/Sql/Internal/Select/FromClause.hs
+++ noided-sql/lib/Noided/Sql/Internal/Select/FromClause.hs
@@ -69,7 +69,7 @@ writeFromClause ff = \case
     selBase <- writeFromClause ff fromBase
     " INNER JOIN "
     let built = buildItem selBase
-    when (fromItemLateralUsage built == SometimesLateral) $ "LATERAL "
+    when (fromItemLateralUsage built == SometimesLateral) "LATERAL "
     writeFromItem built
     " AS "
     joinedItem <- writeFromItemAfterAs built
