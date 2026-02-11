bcd6a62c48f635e75c9a256baab955f863c86923
Anthony Super <anthony@noided.media>
2026-02-11 15:42:51 -0700

    fix

--- noided-web/lib/Noided/Web/Effect/TimeEvent.hs
+++ noided-web/lib/Noided/Web/Effect/TimeEvent.hs
@@ -14,13 +14,20 @@ module Noided.Web.Effect.TimeEvent
 where
 
 import Data.ByteString (ByteString)
+import Data.Map.Strict qualified as Map
+import Data.Text qualified as T
+import Data.Text.Encoding qualified as T
 import Effectful
 import Noided.Web.Internal.Effect.CurrentTime
 import Noided.Web.Internal.Effect.TimeEvent
 import Noided.Web.Internal.Effect.WriteHeader
 
 renderTimingMapHeader :: TimingMap -> ByteString
-renderTimingMapHeader = error "TODO: implement me"
+renderTimingMapHeader tm = T.encodeUtf8 $ T.intercalate ", " $ map renderOne (Map.toList tm)
+  where
+    renderOne (TimedEventName name, dur) =
+      let ms = (realToFrac dur :: Double) * 1000
+       in name <> ";dur=" <> T.pack (show ms)
 
 runLoggingEventTimingsToHeader :: (CurrentTime :> es, WriteHeader :> es) => Eff (TimeEvent : es) b -> Eff es b
 runLoggingEventTimingsToHeader act = do
