b1016a3511bccc5194c042fc05857fc1ab3a5bcd
Anthony Super <anthony@noided.media>
2026-02-20 13:27:17 -0700

    Let gemini go wild

--- noided-sql/lib/Noided/Sql/Internal/Type/AggregateExpr.hs
+++ noided-sql/lib/Noided/Sql/Internal/Type/AggregateExpr.hs
@@ -5,6 +5,7 @@ module Noided.Sql.Internal.Type.AggregateExpr where
 
 import Data.Int
 import Data.Kind
+import Noided.Sql.Internal.Class.SqlNumeric
 import Noided.Sql.Internal.Type.SqlExpr
 import Noided.Sql.Internal.Type.SqlType
 import Noided.Sql.Internal.Type.Syntax
@@ -57,6 +58,47 @@ unsafeBuildSingleArgAgg syn (UnsafeMkSqlExpr re) =
 count_ :: SqlExpr valScope w -> AggregateExpr valScope (NonNullT Int64)
 count_ = unsafeBuildSingleArgAgg @_ @(NonNullT Int64) "COUNT"
 
+-- | Sql @COUNT(DISTINCT ...)@ aggregate function.
+countDistinct_ :: SqlExpr valScope (SqlT n r) -> AggregateExpr valScope (NonNullT Int64)
+countDistinct_ (UnsafeMkSqlExpr re) =
+  UnsafeMkAggExpr {aggExprSyntax = "COUNT(DISTINCT " <> re <> ")", aggExprWhere = NoAggregateFilter}
+
 -- | Sql @MIN@ aggregate function.
-min_ :: SqlExpr valScope (SqlType n r) -> AggregateExpr valScope (NullableT r)
+min_ :: SqlExpr valScope (SqlT n r) -> AggregateExpr valScope (NullableT r)
 min_ = unsafeBuildSingleArgAgg "MIN"
+
+-- | Sql @MAX@ aggregate function.
+max_ :: SqlExpr valScope (SqlT n r) -> AggregateExpr valScope (NullableT r)
+max_ = unsafeBuildSingleArgAgg "MAX"
+
+-- | Sql @SUM@ aggregate function.
+sum_ :: (SqlNumeric r) => SqlExpr valScope (SqlT n r) -> AggregateExpr valScope (NullableT (SumType r))
+sum_ = unsafeBuildSingleArgAgg "SUM"
+
+-- | Sql @AVG@ aggregate function.
+avg_ :: (SqlNumeric r) => SqlExpr valScope (SqlT n r) -> AggregateExpr valScope (NullableT (AvgType r))
+avg_ = unsafeBuildSingleArgAgg "AVG"
+
+-- | Sql @EVERY@ aggregate function (also known as @BOOL_AND@).
+every_ :: SqlExpr valScope (SqlT n Bool) -> AggregateExpr valScope (NullableT Bool)
+every_ = unsafeBuildSingleArgAgg "EVERY"
+
+-- | Sql @ANY@ aggregate function (also known as @BOOL_OR@).
+any_ :: SqlExpr valScope (SqlT n Bool) -> AggregateExpr valScope (NullableT Bool)
+any_ = unsafeBuildSingleArgAgg "ANY"
+
+-- | Sql @BOOL_AND@ aggregate function.
+boolAnd_ :: SqlExpr valScope (SqlT n Bool) -> AggregateExpr valScope (NullableT Bool)
+boolAnd_ = unsafeBuildSingleArgAgg "BOOL_AND"
+
+-- | Sql @BOOL_OR@ aggregate function.
+boolOr_ :: SqlExpr valScope (SqlT n Bool) -> AggregateExpr valScope (NullableT Bool)
+boolOr_ = unsafeBuildSingleArgAgg "BOOL_OR"
+
+-- | Sql @STDDEV@ aggregate function.
+stddev_ :: (SqlNumeric r) => SqlExpr valScope (SqlT n r) -> AggregateExpr valScope (NullableT Double)
+stddev_ = unsafeBuildSingleArgAgg "STDDEV"
+
+-- | Sql @VARIANCE@ aggregate function.
+variance_ :: (SqlNumeric r) => SqlExpr valScope (SqlT n r) -> AggregateExpr valScope (NullableT Double)
+variance_ = unsafeBuildSingleArgAgg "VARIANCE"
