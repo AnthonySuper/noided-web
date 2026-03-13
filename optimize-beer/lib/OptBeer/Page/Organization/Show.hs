aa700e4ea6ff7ba530050648313a665c8aff2df9
Anthony Super <anthony@noided.media>
2026-03-12 18:23:02 -0600

    Replace inline item form with quick actions link on organization page

--- optimize-beer/lib/OptBeer/Page/Organization/Show.hs
+++ optimize-beer/lib/OptBeer/Page/Organization/Show.hs
@@ -4,20 +4,21 @@ module OptBeer.Page.Organization.Show where
 
 import Lucid
 import Noided.Pathname (usePathTemplate)
-import Noided.Translate (MessageKey)
 import Noided.Web.Html (renderTranslated)
-import Noided.Web.Html.FormRender (renderFormT)
 import OptBeer.DB.Table.Organization
-import OptBeer.Form.Render.CreateItem (createItemRenderer)
-import OptBeer.Form.Type.CreateItem (emptyCreateItemForm)
 import OptBeer.Page.Type
-import OptBeer.Routes (createItemPath)
+import OptBeer.Routes (newItemPath)
 import OptBeer.Type.OrganizationIdent (OrganizationIdent (..))
 
 showOrganizationPage :: Organization -> HtmlT Page ()
 showOrganizationPage org =
   div_ [class_ "organization-show-container"] $ do
     h1_ [class_ "organization-name"] $ toHtml org.name
+    
+    div_ [class_ "quick-actions-bar"] $ do
+      a_ [class_ "button", href_ (usePathTemplate newItemPath (OrganizationById org.id))] $
+        renderTranslated ["organization.items.create.link"] mempty
+
     p_ [class_ "organization-id"] $ do
       renderTranslated ["organization.idLabel"] mempty
       " "
@@ -25,12 +26,4 @@ showOrganizationPage org =
 
     div_ [class_ "organization-items-section"] $ do
       h2_ $ renderTranslated ["organization.items.title"] mempty
-      
-      -- For now, just render the "Add Item" form
-      div_ [class_ "create-item-form-container"] $ do
-        h3_ $ renderTranslated ["organization.items.create.title"] mempty
-        form_ [method_ "post", action_ (usePathTemplate createItemPath (OrganizationById org.id)), class_ "form"] $ do
-          renderFormT createItemRenderer emptyCreateItemForm mempty
-          div_ [class_ "form-buttons"] $
-            button_ [class_ "button", type_ "submit"] $
-              renderTranslated (["organization.items.create.button"] :: [MessageKey]) mempty
+      -- Items list will go here later
